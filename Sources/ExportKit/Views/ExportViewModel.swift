f19715a605bea1fab48554aaa344bce9039bd984
Atakan Ozcan <ge79vef@mytum.de>
2023-03-19 22:08:15 +0100

    add export in bulk

--- Sources/ExportKit/Views/ExportViewModel.swift
+++ Sources/ExportKit/Views/ExportViewModel.swift
@@ -41,6 +41,59 @@ class ExportViewModel: ObservableObject {
             alert = .descriptiveError(error.localizedDescription)
         }
     }
+
+    func exportAll() {
+        guard let exportStrategy = ExportKit.shared.exportStrategy else {
+            alert = .descriptiveError("No export strategy")
+            ExportKit.shared.logHandler?("[ EXPORTER ] No export strategy")
+            return
+        }
+
+        guard let topViewController = UIApplication.topViewController() else {
+            alert = .descriptiveError("No Top View Controller")
+            ExportKit.shared.logHandler?("[ EXPORTER ] No Top View Controller")
+            return
+        }
+
+        let results = data.reduce(.success([Item]())) { (partialResult, item) -> Result<[Any], Error> in
+            switch partialResult {
+            case .failure(_):
+                return partialResult
+            case .success(let array):
+                switch exportStrategy(item) {
+                case .success(let itemResult):
+                    return .success(array + CollectionOfOne(itemResult))
+                case .failure(let error):
+                    return .failure(error)
+                }
+            }
+        }
+
+        switch results {
+        case .success(let items):
+            var itemsToShare = [Any]()
+
+            if let textItems = items as? [String] {
+                let textToShare = textItems.reduce("") { partialResult, item in
+                    partialResult == "" ? item : partialResult + "\n" + item
+                }
+                itemsToShare.append(textToShare)
+            } else {
+                itemsToShare.append(items)
+            }
+
+            let activityViewController = UIActivityViewController(activityItems: itemsToShare, applicationActivities: nil)
+
+            if UIDevice.current.userInterfaceIdiom == .pad {
+                activityViewController.popoverPresentationController?.sourceView = topViewController.view
+                activityViewController.popoverPresentationController?.sourceRect = CGRect(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2, width: 0, height: 0)
+                activityViewController.popoverPresentationController?.permittedArrowDirections = []
+            }
+            topViewController.present(activityViewController, animated: true, completion: nil)
+        case .failure(let error):
+            alert = .descriptiveError(error.localizedDescription)
+        }
+    }
     
     func delete(at offsets: IndexSet) {
         offsets.forEach {
